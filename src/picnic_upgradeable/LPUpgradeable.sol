442eaa8e91f065d21fc775f3563c10d92d286acf
KeFuCode <fukeee@163.com>
2022-11-26 22:43:19 +0800

    add/picnic-uups-upgradeable

---
+++ src/picnic_upgradeable/LPUpgradeable.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.7;
+
+import "openzeppelin-contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "openzeppelin-contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "openzeppelin-contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
+import "openzeppelin-contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract LPUpgradeable is
+    Initializable,
+    ERC20Upgradeable,
+    OwnableUpgradeable,
+    AccessControlUpgradeable,
+    UUPSUpgradeable
+{
+    bytes32 public constant SALES_ROLE = keccak256("SALES_ROLE");
+    bytes32 public constant POOL_ROLE = keccak256("POOL_ROLE");
+
+    event IncreaseLP(address indexed account, uint256 indexed amount);
+    event DecreaseLP(address indexed account, uint256 indexed amount);
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(
+        address _sales,
+        address _pool,
+        address _admin
+    ) public initializer {
+        __ERC20_init("Picnic Profit", "PP");
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+
+        _grantRole(SALES_ROLE, _sales);
+        _grantRole(POOL_ROLE, _pool);
+        _grantRole(DEFAULT_ADMIN_ROLE, _admin);
+    }
+
+    function sendLP(address _receiver, uint256 _amount)
+        external
+        onlyRole(SALES_ROLE)
+    {
+        _mint(_receiver, _amount);
+
+        emit IncreaseLP(_receiver, _amount);
+    }
+
+    function burnLP(address _account, uint256 _amount)
+        external
+        onlyRole(POOL_ROLE)
+    {
+        _burn(_account, _amount);
+
+        emit DecreaseLP(_account, _amount);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
+}
---
+++ src/picnic_upgradeable/PoolUpgradeable.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.7;
+
+import "openzeppelin-contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "openzeppelin-contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import "openzeppelin-contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
+import "openzeppelin-contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+import "./interfaces/ILPUpgradeable.sol";
+
+contract PoolUpgradeable is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+    using SafeERC20Upgradeable for IERC20Upgradeable;
+
+    address public LP;
+    address public TokenAddress;
+
+    // ============ Methods ============
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address _LP, address _tokenAddress) public initializer {
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+
+        LP = _LP;
+        TokenAddress = _tokenAddress;
+    }
+
+    function withdrawFunds(uint256 _amount) external {
+        require(
+            ILPUpgradeable(LP).balanceOf(msg.sender) >= _amount,
+            "LP balance is not enough."
+        );
+        ILPUpgradeable(LP).burnLP(msg.sender, _amount);
+        _sendFunds(msg.sender, _amount);
+    }
+
+    function setLP(address _lp) external onlyOwner {
+        LP = _lp;
+    }
+
+    function setTokenAddress(address _tokenAddress) external onlyOwner {
+        TokenAddress = _tokenAddress;
+    }
+
+    // ============ Private Methods ============
+
+    function _sendFunds(address _receiver, uint256 _amount) private {
+        require(
+            IERC20Upgradeable(TokenAddress).balanceOf(address(this)) >= _amount,
+            "Insufficient balance to send"
+        );
+        IERC20Upgradeable(TokenAddress).safeTransfer(_receiver, _amount);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        override
+        onlyOwner
+    {}
+}
